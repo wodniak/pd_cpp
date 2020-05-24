1b2bdd3563142f08e5b2daa50c7ebb994c98e24a
gwozniak <wozgrz96@gmail.com>
2020-05-24 18:58:24 +0100

    vector optimization, style corrected

--- parallel_quick_sort.cpp
+++ parallel_quick_sort.cpp
@@ -8,6 +8,7 @@
 #include <thread>
 #include <future>
 #include <cassert>
+
 struct empty_stack: public std::exception
 {
     const char* what() const throw()
@@ -16,6 +17,7 @@ struct empty_stack: public std::exception
     }
 };
 
+/// Threadsafe wrapper around std::stack
 template<typename T>
 class threadsafe_stack
 {
@@ -67,10 +69,13 @@ public:
     }
 };
 
-
+/// Parallel quick_sort algorithm.
+/// Separates data to chunks and process them in parallel. Implementation based on promises.
 template<typename T>
-struct sorter
+class QuickSorter
 {
+private:
+    /// contains part of data to sort and associated promise. Movable only.
     struct chunk_to_sort
     {
         std::list<T> data;
@@ -86,25 +91,47 @@ struct sorter
     };
 
     std::atomic<bool> end_of_data;
-    unsigned const max_thread_count;
+    const unsigned max_thread_count;
     std::vector<std::thread> threads;
     threadsafe_stack<chunk_to_sort> chunks;
 
-    sorter():
-        max_thread_count(std::thread::hardware_concurrency()-1),
-        end_of_data(false)
-    {}
-
-    ~sorter()
-    {
-        end_of_data=true;
-        for(unsigned i=0;i<threads.size();++i) { threads[i].join(); }
-    }
-
+    /// get new chunk from stack and sort
     void try_sort_chunk()
     {
-        std::shared_ptr<chunk_to_sort> chunk = chunks.pop();
+        std::shared_ptr<chunk_to_sort> chunk = this->chunks.pop();
         if(chunk) { sort_chunk(chunk); }
+    }
+
+    void sort_chunk(const std::shared_ptr<chunk_to_sort>& chunk)
+    {
+        chunk->promise.set_value(do_sort(chunk->data));
+    }
+
+    /// worker thread function
+    void sort_thread()
+    {
+        while(not this->end_of_data)
+        {
+            try_sort_chunk();
+            std::this_thread::yield();
+        }
+    }
+
+public:
+    QuickSorter():
+        max_thread_count(std::thread::hardware_concurrency()-1),
+        end_of_data(false)
+    {
+        this->threads.reserve(this->max_thread_count);
+    }
+
+    ~QuickSorter()
+    {
+        this->end_of_data = true;
+        for(unsigned i = 0; i < this->threads.size(); ++i) 
+        { 
+            if(this->threads[i].joinable()) { this->threads[i].join(); } 
+        }
     }
 
     std::list<T> do_sort(std::list<T>& chunk_data)
@@ -112,21 +139,26 @@ struct sorter
         if(chunk_data.empty()) { return chunk_data; }
 
         std::list<T> result;
-        result.splice(result.begin(),chunk_data,chunk_data.begin());
-        T const& partition_val=*result.begin();
+        result.splice(result.begin(), chunk_data, chunk_data.begin());
+        const T& partition_val =* result.begin();
 
-        typename std::list<T>::iterator divide_point = std::partition(chunk_data.begin(),chunk_data.end(),
-                                                           [&](T const& val){return val<partition_val;});
+        /// divide data based on pivot
+        typename std::list<T>::iterator divide_point = std::partition(chunk_data.begin(), chunk_data.end(),
+                                                           [&](const T& val){return val < partition_val;});
         chunk_to_sort new_lower_chunk;
-        new_lower_chunk.data.splice(new_lower_chunk.data.end(), chunk_data,chunk_data.begin(),
-                                    divide_point);
+        new_lower_chunk.data.splice(new_lower_chunk.data.end(), chunk_data,chunk_data.begin(), divide_point);
 
         std::future<std::list<T>> new_lower = new_lower_chunk.promise.get_future();
-        chunks.push(std::move(new_lower_chunk));
-        if(threads.size()<max_thread_count) { threads.push_back(std::thread(&sorter<T>::sort_thread,this)); }
+        this->chunks.push(std::move(new_lower_chunk));
         
-        std::list<T> new_higher(do_sort(chunk_data));
-        result.splice(result.end(),new_higher);
+        /// spawn new worker if possible
+        if(this->threads.size() < this->max_thread_count) 
+        { 
+            this->threads.emplace_back(&QuickSorter<T>::sort_thread, this);
+        }
+        
+        std::list<T> new_higher(this->do_sort(chunk_data));
+        result.splice(result.end(), new_higher);
         while(new_lower.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
         {
             try_sort_chunk();
@@ -135,27 +167,13 @@ struct sorter
         result.splice(result.begin(),new_lower.get());
         return result;
     }
-
-    void sort_chunk(std::shared_ptr<chunk_to_sort > const& chunk)
-    {
-        chunk->promise.set_value(do_sort(chunk->data));
-    }
-
-    void sort_thread()
-    {
-        while(!end_of_data)
-        {
-            try_sort_chunk();
-            std::this_thread::yield();
-        }
-    }
 };
 
 template<typename T>
 std::list<T> parallel_quick_sort(std::list<T> input)
 {
     if(input.empty()) { return input; }
-    sorter<T> s;
+    QuickSorter<T> s;
     return s.do_sort(input);
 }
 
